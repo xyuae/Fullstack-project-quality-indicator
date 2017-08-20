67829c63adc31ae9ecae44e48a760822fa0b1b37
Tony <xyuae@connect.ust.hk>
2017-08-20 13:08:48 +0800

    first commit

---
+++ server/tigers.js
@@ -0,0 +1,60 @@
+// TODO: make a new router for the tigers resource
+// and make some REST routes for it, exactly like for lions
+// make a middleware that just logs the word 'tiger' to the console
+// when a request comes in to the server
+var tigerRouter = require('express').Router();
+
+var tigers = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+	if (!req.body.id) {
+		id++;
+		req.body.id = id + '';
+	}
+	next();
+};
+
+tigerRouter.param('id', function(req, res, next, id) {
+	var tiger = _.find(tigers, {id: id});
+
+	if (tiger) {
+		req.tiger = tiger;
+		next()
+	} else {
+		res.send();
+	}
+});
+
+tigerRouter.get('/', (req, res) => {
+	res.json(tigers);
+});
+
+tigerRouter.get('/:id', (req, res) => {
+	var tiger = req.tiger;
+	res.json(tiger || {});
+});
+
+tigerRouter.post('/', updateId, (req, res) => {
+	var tiger = req.body;
+
+	tigers.push(tiger);
+	res.json(tiger);
+});
+
+tigerRouter.put('/:id', (req, res) => {
+	var update = req.body;
+	if(update.id) {
+		delete update.id
+	}
+
+	var tiger = _.findIndex(tigers, {id: req.params.id});
+	if (!tigers[tiger]) {
+		res.send();
+	} else {
+		var updatedTiger = _.assign(tigers[tiger], update);
+		res.json(updatedTiger);
+	}
+});
+
+module.exports = tigerRouter;
