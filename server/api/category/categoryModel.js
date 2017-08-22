f5fd48437d960042cbc97ea8f9bfe16923b1ebee
Tony <xyuae@connect.ust.hk>
2017-08-22 19:40:13 +0800

    data model

---
+++ server/api/category/categoryModel.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+
+});
+
+module.exports = mongoose.model('category', CategorySchema);
---
+++ server/api/user/userModel.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+
+});
+
+module.exports = mongoose.model('user', UserSchema);
