2bf58391e02be04b01237c149f55b0a107997eab
Tony <xyuae@connect.ust.hk>
2017-08-22 15:17:05 +0800

    change organization

--- server/server.js
+++ server/server.js
@@ -1,30 +1,14 @@
-
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var _ = require('lodash');
-var morgan = require('morgan');
+var api = require('./api/api');
+var err = require('./middleware/err');
 
-var lionRouter = require('./lions');
-var tigerRouter = require('./tigers');
-var projectRouter = require('./project')
+// setup the app middlware
+require('./middleware/appMiddlware')(app);	//IIFE
 
-
-app.use(morgan('dev'))  // show http request in consle
-app.use(express.static('client'));  // make client file public
-app.use(bodyParser.urlencoded({extended: true})); // bodyParser middleware
-app.use(bodyParser.json()); // read json middleware
-// this is called mounting. when ever a req comes in for
-// '/projects' we want to use this router
-app.use('/lions', lionRouter);
-app.use('/projects', projectRouter);    // mount project router
-
-app.use(function(err, req, res, next) { // handle error
-  if (err) {
-    res.status(500).send(err);
-  }
-});
-
-
-app.listen(3000);
-console.log('on port 3000');
+// setup the api
+app.use('/api', api);
+// set up global error handling
+app.use(err);
+// export the app for testing
+module.exports = app;
